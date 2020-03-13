61d2b185d554159b8bf477b5154746fd1e6af8d5
hisayuki <siruvalice@gmail.com>
2020-03-13 14:12:51 +0900

    rollupの修正

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,53 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import html from "rollup-plugin-bundle-html"
+import html from 'rollup-plugin-bundle-html';
 import { terser } from 'rollup-plugin-terser';
-import postcss from "rollup-plugin-postcss"
-import typescript from "rollup-plugin-typescript2"
-import typescriptCompiler from "typescript"
-import sveltePreprocessor from "svelte-preprocess"
+import postcss from 'rollup-plugin-postcss';
+import serve from 'rollup-plugin-serve';
+import dotenv from 'dotenv';
+import typescript from 'rollup-plugin-typescript2';
+import typescriptCompiler from 'typescript';
+import sveltePreprocessor from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 
-const production = !process.env.ROLLUP_WATCH;
+dotenv.config();
+const plugins = [
+	svelte({
+		dev: process.env.DEVELOP_MODE,
+		css: false,
+		preprocess: sveltePreprocessor(),
+		emitCss: true
+	}),
+	html({
+		template: "src/index.html",
+		dest: "dist",
+		filename: "index.html"
+	}),
+	postcss({
+		extract: './dist/css/bundle.css',
+		sourceMap: true,
+		plugins: [
+			autoprefixer()
+		]
+	}),
+	typescript({ typescript: typescriptCompiler }),
+	resolve(),
+	commonjs()
+]
+
+if (process.env.DEVELOP_MODE) {
+	plugins.push(
+		serve({
+			contentBase: './dist',
+			open: true
+		}),
+		livereload({ watch: "./dist" })
+	);
+} else {
+	plugins.push(terser({ sourcemap: true }))
+}
+
 
 export default {
 	input: 'src/main.ts',
@@ -20,50 +58,8 @@ export default {
 		name: 'app',
 		file: './dist/js/bundle.js'
 	},
-	plugins: [
-		svelte({
-			dev: !production,
-			css: false,
-			preprocess: sveltePreprocessor(),
-			emitCss: true
-		}),
-		html({
-			template: "src/index.html",
-			dest: "dist",
-			filename: "index.html"
-		}),
-		postcss({
-			extract: './dist/css/bundle.css',
-			sourceMap: true,
-			plugins: [
-				autoprefixer()
-			]
-		}),
-		typescript({ typescript: typescriptCompiler }),
-		resolve(),
-		commonjs(),
-		!production && serve(),
-		!production && livereload({ watch: "./dist" }),
-		production && terser()
-	],
+	plugins,
 	watch: {
 		clearScreen: false
 	}
 };
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
-}
